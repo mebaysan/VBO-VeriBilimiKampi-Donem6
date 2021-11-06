b8ea209f312351c89c43a552974e83da54fec6a9
mebaysan <menesbaysan@gmail.com>
2021-11-06 15:54:30 +0300

    hafta 10 baslangic

---
+++ Hafta-10/ders/01-nlp-amazon-reviews.py
@@ -0,0 +1,358 @@
+##################################################
+# Introduction to Text Mining and Natural Language Processing
+##################################################
+
+##################################################
+# Sentiment Analysis and Sentiment Modeling for Amazon Reviews
+##################################################
+
+
+# 1. Text Preprocessing
+# 2. Text Visualization
+# 3. Sentiment Analysis
+# 4. Feature Engineering
+# 5. Sentiment Modeling
+
+
+from warnings import filterwarnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, GridSearchCV, cross_validate
+from sklearn.preprocessing import LabelEncoder
+from textblob import Word, TextBlob
+from wordcloud import WordCloud
+
+
+filterwarnings('ignore')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 200)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+
+##################################################
+# 1. Text Preprocessing
+##################################################
+
+df = pd.read_csv("datasets/amazon_reviews.csv", sep=",")
+df.head()
+df.info()
+
+###############################
+# Normalizing Case Folding
+###############################
+
+df['reviewText'] = df['reviewText'].str.lower()
+
+###############################
+# Punctuations
+###############################
+
+df['reviewText'] = df['reviewText'].str.replace('[^\w\s]', '')
+
+###############################
+# Numbers
+###############################
+
+df['reviewText'] = df['reviewText'].str.replace('\d', '')
+
+###############################
+# Stopwords
+###############################
+
+# nltk.download('stopwords')
+sw = stopwords.words('english')
+df['reviewText'] = df['reviewText'].apply(lambda x: " ".join(x for x in str(x).split() if x not in sw))
+
+###############################
+# Rarewords
+###############################
+
+drops = pd.Series(' '.join(df['reviewText']).split()).value_counts()[-1000:]
+df['reviewText'] = df['reviewText'].apply(lambda x: " ".join(x for x in x.split() if x not in drops))
+
+###############################
+# Tokenization
+###############################
+
+# nltk.download("punkt")
+df["reviewText"].apply(lambda x: TextBlob(x).words).head()
+
+###############################
+# Lemmatization
+###############################
+
+# Kelimeleri köklerine ayırma işlemidir.
+# nltk.download('wordnet')
+df['reviewText'] = df['reviewText'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+
+df['reviewText'].head(10)
+
+##################################################
+# 2. Text Visualization
+##################################################
+
+###############################
+# Terim Frekanslarının Hesaplanması
+###############################
+
+tf = df["reviewText"].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
+
+tf.columns = ["words", "tf"]
+tf.head()
+tf.shape
+tf["words"].nunique()
+tf["tf"].describe([0.05, 0.10, 0.25, 0.50, 0.75, 0.80, 0.90, 0.95, 0.99]).T
+
+###############################
+# Barplot
+###############################
+
+tf[tf["tf"] > 500].plot.bar(x="words", y="tf")
+plt.show()
+
+###############################
+# Wordcloud
+###############################
+
+text = " ".join(i for i in df.reviewText)
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+# daha açık renkli bir grafik
+wordcloud = WordCloud(max_font_size=50,
+                      max_words=100,
+                      background_color="white").generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud.to_file("wordcloud.png")
+
+###############################
+# Şablonlara Göre Wordcloud
+###############################
+
+vbo_mask = np.array(Image.open("notes/HAFTA_11/img/tr.png"))
+
+wc = WordCloud(background_color="white",
+               max_words=1000,
+               mask=vbo_mask,
+               contour_width=3,
+               contour_color="firebrick")
+
+wc.generate(text)
+plt.figure(figsize=[10, 10])
+plt.imshow(wc, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wc.to_file("vbo.png")
+
+##################################################
+# 3. Sentiment Analysis
+##################################################
+
+# NLTK already has a built-in, pretrained sentiment analyzer
+# called VADER (Valence Aware Dictionary and sEntiment Reasoner).
+
+df.head()
+# nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+sia.polarity_scores("The film was awesome")
+
+
+sia.polarity_scores("I liked this music but it is not good as the other one")
+
+# mesela review'ları büyültmek istersek:
+df["reviewText"].apply(lambda x: x.upper())
+
+# şimdi skorları hesaplayalım mesela 10 tanesi için
+df["reviewText"][0:10].apply(lambda x: sia.polarity_scores(x))
+
+# peki bu sözlük içerisinden sadece bir bileşeni seçmek istersek ne yapacağız?
+df["reviewText"][0:10].apply(lambda x: sia.polarity_scores(x)["compound"])
+
+# işlemi kalıcı olarak yapalım:
+df["polarity_score"] = df["reviewText"].apply(lambda x: sia.polarity_scores(x)["compound"])
+df.head()
+
+
+##################################################
+# 4. Sentiment Modeling
+##################################################
+
+###############################
+# Feature Engineering
+###############################
+
+###############################
+# Target'ın Oluşturulması
+###############################
+
+df["reviewText"][0:10].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+
+# şimdi tüm veri için aynı işlemi yapıp veri setinin içine sentiment_label adında bir değişken ekleyelim:
+df["sentiment_label"] = df["reviewText"].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+df.head(20)
+
+# dengesiz veri problemimiz var mı bir sınıf dağılımına bakalım
+df["sentiment_label"].value_counts()
+
+# bir soru daha merak ettiğim şey şu verilen puanlar açısından neg-pos labelleri arasında fark var mı?
+df.groupby("sentiment_label")["overall"].mean()
+
+# target'ın encode edilmesi
+df["sentiment_label"] = LabelEncoder().fit_transform(df["sentiment_label"])
+
+X = df["reviewText"]
+y = df["sentiment_label"]
+
+
+# ngram
+a = """Bu örneği anlaşılabilmesi için daha uzun bir metin üzerinden göstereceğim.
+N-gram'lar birlikte kullanılan kelimelerin kombinasyolarını gösterir ve feature üretmek için kullanılır"""
+
+TextBlob(a).ngrams(3)
+
+
+###############################
+# Count Vectors
+###############################
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+corpus = ['This is the first document.',
+          'This document is the second document.',
+          'And this is the third one.',
+          'Is this the first document?']
+
+# word frekans
+vectorizer = CountVectorizer()
+X_c = vectorizer.fit_transform(corpus)
+vectorizer.get_feature_names()
+X_c.toarray()
+
+# n-gram frekans
+vectorizer2 = CountVectorizer(analyzer='word', ngram_range=(2, 2))
+X_n = vectorizer2.fit_transform(corpus)
+vectorizer2.get_feature_names()
+X_n.toarray()
+
+
+# Veriye uygulanması:
+vectorizer = CountVectorizer()
+X_count = vectorizer.fit_transform(X)
+
+vectorizer.get_feature_names()[10:15]
+X_count.toarray()[10:15]
+
+
+###############################
+# TF-IDF
+###############################
+
+# word tf-idf
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(analyzer='word')
+X_w = vectorizer.fit_transform(corpus)
+vectorizer.get_feature_names()
+X_w.toarray()
+
+# n-gram tf-idf
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(ngram_range=(2, 3))
+X_n = vectorizer.fit_transform(corpus)
+vectorizer.get_feature_names()
+X_n.toarray()
+
+
+# Veriye uygulanması:
+tf_idf_word_vectorizer = TfidfVectorizer()
+X_tf_idf_word = tf_idf_word_vectorizer.fit_transform(X)
+
+# Veriye uygulanması:
+tf_idf_ngram_vectorizer = TfidfVectorizer(ngram_range=(2, 3))
+X_tf_idf_ngram = tf_idf_word_vectorizer.fit_transform(X)
+
+
+###############################
+# 5. Modeling
+###############################
+
+###############################
+# Logistic Regression
+###############################
+
+log_model = LogisticRegression().fit(X_tf_idf_word, y)
+
+cross_val_score(log_model,
+                X_tf_idf_word,
+                y, scoring="accuracy",
+                cv=5).mean()
+
+yeni_yorum = pd.Series("this product is great")
+yeni_yorum = pd.Series("look at that shit very bad")
+yeni_yorum = pd.Series("it was good but I am sure that it fits me")
+
+yeni_yorum = CountVectorizer().fit(X).transform(yeni_yorum)
+log_model.predict(yeni_yorum)
+
+# orjinal yorumlardan modele sorabilir miyiz?
+random_review = pd.Series(df["reviewText"].sample(1).values)
+random_review
+
+yeni_yorum = CountVectorizer().fit(X).transform(random_review)
+log_model.predict(yeni_yorum)
+
+
+###############################
+# Random Forests
+###############################
+
+# Count Vectors
+rf_model = RandomForestClassifier().fit(X_count, y)
+cross_val_score(rf_model, X_count, y, cv=5, n_jobs=-1).mean()
+
+# TF-IDF Word-Level
+rf_model = RandomForestClassifier().fit(X_tf_idf_word, y)
+cross_val_score(rf_model, X_tf_idf_word, y, cv=5, n_jobs=-1).mean()
+
+# TF-IDF N-GRAM
+rf_model = RandomForestClassifier().fit(X_tf_idf_ngram, y)
+cross_val_score(rf_model, X_tf_idf_ngram, y, cv=5, n_jobs=-1).mean()
+
+###############################
+# Hiperparametre Optimizasyonu
+###############################
+
+rf_model = RandomForestClassifier(random_state=17)
+
+rf_params = {"max_depth": [5, 8, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [2, 5, 8, 20],
+             "n_estimators": [100, 200, 500]}
+
+rf_best_grid = GridSearchCV(rf_model,
+                            rf_params,
+                            cv=5,
+                            n_jobs=-1,
+                            verbose=True).fit(X_count, y)
+
+rf_best_grid.best_params_
+
+rf_final = rf_model.set_params(**rf_best_grid.best_params_, random_state=17).fit(X_count, y)
+
+cv_results = cross_validate(rf_final, X_count, y, cv=3, scoring=["accuracy", "f1", "roc_auc"])
+
+cv_results['test_accuracy'].mean()
+cv_results['test_f1'].mean()
+cv_results['test_roc_auc'].mean()
---
+++ Hafta-10/ders/02-pyspark-churn-prediction.py
@@ -0,0 +1,548 @@
+##################################################
+# Churn Prediction using PySpark
+##################################################
+
+# Şirketi terk edecek müşterileri tahmin edebilecek bir makine öğrenmesi modeli geliştirebilir misiniz?
+# Amaç bir bankanın müşterilerinin bankayı terk etme ya da terk etmeme durumunun tahmin edilmesidir.
+
+# 1. Kurulum
+# 2. Exploratory Data Analysis
+# 3. SQL Sorguları
+# 4. Data Preprocessing & Feature Engineering
+# 5. Modeling
+
+##################################################
+# Kurulum
+##################################################
+
+# https://spark.apache.org/downloads.html
+# username/spark dizinin altına indir.
+
+# pyarrow: Farklı veri yapıları arasında çalışma kolaylığı sağlayan bir modul.
+
+# pip install pyspark
+# pip install findspark
+# pip install PyArrow
+
+
+import warnings
+import findspark
+import pandas as pd
+import seaborn as sns
+from pyspark.ml.classification import GBTClassifier, LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import StringIndexer, VectorAssembler, OneHotEncoder, StandardScaler
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import Bucketizer
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+findspark.init("/Users/simgeerek/Documents/Spark/spark-3.1.2-bin-hadoop3.2")
+
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("pyspark_giris") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+# http://mvahit-mbp.lan:4040/jobs/
+# sc.stop()
+
+##################################################
+# Exploratory Data Analysis
+##################################################
+
+
+############################
+# Pandas df ile Spark df farkını anlamak.
+############################
+
+spark_df = spark.read.csv("datasets/churn.csv", header=True, inferSchema=True)
+type(spark_df)
+
+
+df = sns.load_dataset("diamonds")
+type(df)
+
+
+df.head()
+spark_df.head()
+
+df.dtypes
+spark_df.dtypes
+
+df.ndim
+# spark_df.ndim
+
+
+# Reading a json file
+# df = spark.read.json(json_file_path)
+#
+# # Reading a text file
+# df = spark.read.text(text_file_path)
+
+# # Reading a parquet file
+# df = spark.read.load(parquet_file_path) # or
+# df = spark.read.parquet(parquet_file_path)
+
+
+############################
+# Exploratory Data Analysis
+############################
+
+# Gözlem ve değişken sayısı
+print("Shape: ", (spark_df.count(), len(spark_df.columns)))
+
+# Değişken tipleri
+spark_df.printSchema()
+spark_df.dtypes
+
+# Değişken seçme
+spark_df.Age
+
+
+# Bir değişkeni görmek
+spark_df.select(spark_df.Age).show()
+spark_df.take(5)
+spark_df.head()
+spark_df.show()
+
+# Değişken isimlerinin küçültülmesi
+spark_df = spark_df.toDF(*[c.lower() for c in spark_df.columns])
+
+
+# özet istatistikler
+spark_df.describe().show()
+
+
+# sadece belirli değişkenler için özet istatistikler
+spark_df.describe(["age", "churn"]).show()
+
+
+# Kategorik değişken sınıf istatistikleri
+spark_df.groupby("churn").count().show()
+
+
+# Eşsiz sınıflar
+spark_df.select("churn").distinct().show()
+
+
+# select(): Değişken seçimi
+spark_df.select("age", "names").show(5)
+
+
+# filter(): Gözlem seçimi / filtreleme
+spark_df.filter(spark_df.age > 40).show()
+spark_df.filter(spark_df.age > 40).count()
+
+
+# groupby işlemleri
+spark_df.groupby("churn").count().show()
+spark_df.groupby("churn").agg({"age": "mean"}).show()
+
+
+# Tüm numerik değişkenlerin seçimi ve özet istatistikleri
+num_cols = [col[0] for col in spark_df.dtypes if col[1] != 'string']
+spark_df.select(num_cols).describe().show()
+
+spark_df.select(num_cols).describe().toPandas().transpose()
+
+
+# Tüm kategorik değişkenlerin seçimi ve özeti
+cat_cols = [col[0] for col in spark_df.dtypes if col[1] == 'string']
+
+for col in cat_cols:
+    spark_df.select(col).distinct().show()
+
+# Churn'e göre sayısal değişkenlerin özet istatistikleri
+for col in num_cols:
+    spark_df.groupby("churn").agg({col: "mean"}).show()
+
+
+##################################################
+# SQL Sorguları
+##################################################
+
+
+spark_df.createOrReplaceTempView("tbl_df")
+
+spark.sql("show databases").show()
+
+spark.sql("show tables").show()
+
+spark.sql("select age from tbl_df limit 5").show()
+
+spark.sql("select churn, avg(age) from tbl_df group by Churn").show()
+
+
+##################################################
+# Data Preprocessing & Feature Engineering
+##################################################
+
+############################
+# Missing Values
+############################
+
+from pyspark.sql.functions import when, count, col
+spark_df.select([count(when(col(c).isNull(), c)).alias(c) for c in spark_df.columns]).toPandas().T
+
+
+# eksik değere sahip satırları silmek
+spark_df.dropna().show()
+
+
+# tüm veri setindeki eksiklikleri belirli bir değerle doldurmak
+spark_df.fillna(50).show()
+
+
+# eksik değerleri değişkenlere göre doldurmak
+spark_df.na.fill({'age': 50, 'names': 'unknown'}).show()
+
+
+
+
+############################
+# Feature Interaction
+############################
+
+
+spark_df = spark_df.withColumn('age_total_purchase', spark_df.age / spark_df.total_purchase)
+spark_df.show(5)
+
+
+############################
+# Bucketization / Bining / Num to Cat
+############################
+
+
+spark_df.select('age').describe().toPandas().transpose()
+
+
+bucketizer = Bucketizer(splits=[0, 35, 45, 65], inputCol="age", outputCol="age_cat")
+
+spark_df = bucketizer.setHandleInvalid("keep").transform(spark_df)
+
+spark_df.show(20)
+
+
+spark_df = spark_df.withColumn('age_cat', spark_df.age_cat + 1)
+
+
+spark_df.groupby("age_cat").count().show()
+
+
+spark_df.groupby("age_cat").agg({'churn': "mean"}).show()
+
+
+spark_df = spark_df.withColumn("age_cat", spark_df["age_cat"].cast("integer"))
+
+
+spark_df.groupby("age_cat").agg({'churn': "mean"}).show()
+
+
+############################
+# when ile Değişken Türetmek (segment)
+############################
+
+spark_df = spark_df.withColumn('segment', when(spark_df['years'] < 5, "segment_b").otherwise("segment_a"))
+
+
+############################
+# when ile Değişken Türetmek (age_cat_2)
+############################
+
+spark_df.withColumn('age_cat_2',
+                    when(spark_df['age'] < 36, "young").
+                    when((35 < spark_df['age']) & (spark_df['age'] < 46), "mature").
+                    otherwise("senior")).show()
+
+
+
+############################
+# Label Encoding
+############################
+
+
+spark_df.show(5)
+
+
+indexer = StringIndexer(inputCol="segment", outputCol="segment_label")
+
+indexer.fit(spark_df).transform(spark_df).show(5)
+
+
+temp_sdf = indexer.fit(spark_df).transform(spark_df)
+
+spark_df = temp_sdf.withColumn("segment_label", temp_sdf["segment_label"].cast("integer"))
+
+spark_df = spark_df.drop('segment')
+
+############################
+# One Hot Encoding
+############################
+
+encoder = OneHotEncoder(inputCols=["age_cat"], outputCols=["age_cat_ohe"])
+spark_df = encoder.fit(spark_df).transform(spark_df)
+
+spark_df.show(5)
+
+
+############################
+# TARGET'ın Tanımlanması
+############################
+
+
+stringIndexer = StringIndexer(inputCol='churn', outputCol='label')
+temp_sdf = stringIndexer.fit(spark_df).transform(spark_df)
+spark_df = temp_sdf.withColumn("label", temp_sdf["label"].cast("integer"))
+spark_df.show(5)
+
+
+############################
+# Feature'ların Tanımlanması
+############################
+
+cols = ['age', 'total_purchase', 'account_manager', 'years',
+        'num_sites', 'age_total_purchase', 'segment_label', 'age_cat_ohe']
+
+
+va = VectorAssembler(inputCols=cols, outputCol="features")
+va_df = va.transform(spark_df)
+va_df.show()
+
+
+# Final sdf
+final_df = va_df.select("features", "label")
+final_df.show(5)
+
+# StandardScaler
+# scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures")
+# final_df = scaler.fit(final_df).transform(final_df)
+
+
+
+# Split the dataset into test and train sets.
+train_df, test_df = final_df.randomSplit([0.7, 0.3], seed=17)
+train_df.show(10)
+test_df.show(10)
+
+print("Training Dataset Count: " + str(train_df.count()))
+print("Test Dataset Count: " + str(test_df.count()))
+
+
+# spark.createDataFrame()
+
+
+
+
+##################################################
+# Modeling
+##################################################
+
+############################
+# Logistic Regression
+############################
+
+log_model = LogisticRegression(featuresCol='features', labelCol='label').fit(train_df)
+y_pred = log_model.transform(test_df)
+y_pred.show()
+
+
+y_pred.select("label", "prediction").show()
+
+
+# accuracy
+y_pred.filter(y_pred.label == y_pred.prediction).count() / y_pred.count()
+
+
+evaluator = BinaryClassificationEvaluator(labelCol="label", rawPredictionCol="prediction", metricName='areaUnderROC')
+evaluatorMulti = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction")
+
+acc = evaluatorMulti.evaluate(y_pred, {evaluatorMulti.metricName: "accuracy"})
+precision = evaluatorMulti.evaluate(y_pred, {evaluatorMulti.metricName: "precisionByLabel"})
+recall = evaluatorMulti.evaluate(y_pred, {evaluatorMulti.metricName: "recallByLabel"})
+f1 = evaluatorMulti.evaluate(y_pred, {evaluatorMulti.metricName: "f1"})
+roc_auc = evaluator.evaluate(y_pred)
+
+print("accuracy: %f, precision: %f, recall: %f, f1: %f, roc_auc: %f" % (acc, precision, recall, f1, roc_auc))
+
+
+
+############################
+# Gradient Boosted Tree Classifier
+############################
+
+gbm = GBTClassifier(maxIter=100, featuresCol="features", labelCol="label")
+gbm_model = gbm.fit(train_df)
+y_pred = gbm_model.transform(test_df)
+y_pred.show(5)
+
+y_pred.filter(y_pred.label == y_pred.prediction).count() / y_pred.count()
+
+
+
+
+############################
+# Model Tuning
+############################
+
+evaluator = BinaryClassificationEvaluator()
+
+
+gbm_params = (ParamGridBuilder()
+              .addGrid(gbm.maxDepth, [2, 4, 6])
+              .addGrid(gbm.maxBins, [20, 30])
+              .addGrid(gbm.maxIter, [10, 20])
+              .build())
+
+
+
+cv = CrossValidator(estimator=gbm,
+                    estimatorParamMaps=gbm_params,
+                    evaluator=evaluator,
+                    numFolds=5)
+
+cv_model = cv.fit(train_df)
+
+
+y_pred = cv_model.transform(test_df)
+ac = y_pred.select("label", "prediction")
+ac.filter(ac.label == ac.prediction).count() / ac.count()
+
+
+############################
+# New Prediction
+############################
+
+
+names = pd.Series(["Ali Ahmetoğlu", "Taner Gün", "Berkay", "Polat Konak", "Kamil Atasoy"])
+age = pd.Series([18, 43, 34, 50, 40])
+total_purchase = pd.Series([5000, 10000, 6000, 30000, 100000])
+account_manager = pd.Series([1, 0, 0, 1, 1])
+years = pd.Series([20, 10, 3, 8, 30])
+num_sites = pd.Series([2, 8, 8, 6, 50])
+age_total_purchase = age / total_purchase
+segment_label = pd.Series([1, 1, 0, 1, 1])
+age_cat_ohe = pd.Series([1, 1, 0, 2, 1])
+
+
+
+yeni_musteriler = pd.DataFrame({
+    'names': names,
+    'age': age,
+    'total_purchase': total_purchase,
+    'account_manager': account_manager,
+    'years': years,
+    'num_sites': num_sites,
+    "age_total_purchase": age_total_purchase,
+    "segment_label": segment_label,
+    "age_cat_ohe": age_cat_ohe})
+
+
+yeni_sdf = spark.createDataFrame(yeni_musteriler)
+
+new_customers = va.transform(yeni_sdf)
+new_customers.show(3)
+
+results = cv_model.transform(new_customers)
+results.select("names", "prediction").show()
+
+
+# scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures")
+# new_customers_final = scaler.fit(new_customers).transform(new_customers)
+# results = cv_model.transform(new_customers_final)
+# results.select("names", "prediction").show()
+
+
+# sc.stop()
+
+
+
+##################################################
+# BONUS: User Defined Functions (UDFs)
+##################################################
+
+def age_converter(age):
+    if age < 35:
+        return 1
+    elif age < 45:
+        return 2
+    elif age <= 65:
+        return 3
+
+
+
+from pyspark.sql.types import IntegerType, StringType, FloatType
+from pyspark.sql.functions import udf
+
+func_udf = udf(age_converter, IntegerType())
+
+
+spark_df = spark_df.withColumn('age_cat2', func_udf(spark_df['age']))
+
+spark_df.show(5)
+
+spark_df.groupby("age_cat2").count().show()
+
+cat_cols.append('age_cat2')
+
+
+def age_converter(age):
+    if age < 35:
+        return "young"
+    elif age < 45:
+        return "mature"
+    elif age <= 65:
+        return "senior"
+
+
+func_udf = udf(age_converter, StringType())
+spark_df = spark_df.withColumn('age_cat3', func_udf(spark_df['age']))
+spark_df.show(5)
+
+
+def segment(years):
+    if years < 5:
+        return "segment_b"
+    else:
+        return "segment_a"
+
+
+func_udf = udf(segment, StringType())
+spark_df = spark_df.withColumn('segment', func_udf(spark_df['years']))
+
+
+##################################################
+# Pandas UDFs
+##################################################
+# Creates a pandas user defined function (a.k.a. vectorized user defined function).
+
+spark_df.show(5)
+
+spark_df.withColumn('age_square', spark_df.age ** 2).show()
+
+
+
+from pyspark.sql.functions import pandas_udf
+
+@pandas_udf(FloatType())
+def pandas_square(col):
+    return col ** 2
+
+spark_df.select(pandas_square(spark_df.age)).show()
+
+
+@pandas_udf(FloatType())
+def pandas_log(col):
+    import numpy as np
+    return np.log(col)
+
+spark_df.withColumn('age_log', pandas_log(spark_df.age)).show()
